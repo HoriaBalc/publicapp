c0a7e96a86712a00d21b3726570d0af9168be122
HoriaBalc <balc_horia@yahoo.com>
2022-11-08 14:05:01 +0200

    optional assigment

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,11 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-<<<<<<< HEAD
-Console.WriteLine("mona");
-=======
-Console.WriteLine("mike");
->>>>>>> 749920b99897a61e7107e1b430770b42e1171b1e
-Console.WriteLine("mona task");
-Console.WriteLine("task mike");
+
+//string xmlText="<app><app><app></app></app>";
+//string xmlText = "<app></app></app><app><app>";
+string xmlText = "</app><app><app></app></app>";
+int mistakes = 0;
+int open = 0;
+int close = 0;
+string[] tags=xmlText.Split('<');
+
+foreach (string tag in tags) {
+    if (string.Compare(tag, "app>") == 0) {
+        open++;
+    }
+    if (string.Compare(tag, "/app>") == 0)
+    {
+        if (open <= close) {
+            mistakes++;
+        }
+        else {  close++;}
+       
+    }
+}
+
+if (open > close) {
+    mistakes += open - close;
+}
+
+Console.WriteLine(mistakes);
+
+TimeSpan timeSpan = new TimeSpan(0,0,0);
+DateTime date = DateTime.Now;
+DateTime date1 = DateTime.UtcNow;
+Console.WriteLine("Romania time: " + date + " vs Great Britan time:" + date1);
+Console.WriteLine(timeSpan);
+DateTimeOffset dateOffset = new DateTimeOffset(date1, timeSpan);
+Console.WriteLine(dateOffset);
 
 
