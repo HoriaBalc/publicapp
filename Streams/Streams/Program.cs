2a6f2fcdce153ca598b63e179bcf9929860b1b19
HoriaBalc <balc_horia@yahoo.com>
2022-11-17 19:39:45 +0200

    streams-update

--- Streams/Streams/Program.cs
+++ Streams/Streams/Program.cs
@@ -1,14 +1,31 @@
-﻿using System.IO.Compression;
+﻿using Streams;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Reflection.PortableExecutable;
 using System.Security.Cryptography;
+using System.Text;
 using static System.Console;
 
+
+
+
+
+
 //Write some data
+List <Sport> sports = new List <Sport> ();
+sports.Add(new Sport("running"));
+sports.Add(new Sport("cycling"));
+sports.Add(new Sport("hiking"));
+
+
 using (StreamWriter writer = new StreamWriter(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\data.txt")) 
 {
     var list = new List<string>();
-    for(int i = 1; i < 11; i++)
+    foreach(Sport sport in sports)
     {
-       list.Add("name" + i);
+       var line = sport.Name;  
+       list.Add(line);
     }
     foreach (string s in list) { 
         writer.WriteLine(s);
@@ -16,30 +33,50 @@ using (StreamWriter writer = new StreamWriter(@"C:\Users\Roxy\Documents\AMDARIS\
    
 }
 
+//Read some data
+//List <Sport> sports2 = new List <Sport> ();
+//using (StreamReader reader = new StreamReader(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\data.txt"))
+//{
+//    while (reader.EndOfStream == false)
+//    {
+//            sports2.Add(new Sport(reader.ReadLine()));
+//    }
+
+//}q
+
 //Compress that data
 using (Stream data = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\data.txt", FileMode.OpenOrCreate))
-    using (Stream s = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\GZipCompressed.txt", FileMode.OpenOrCreate))
+using (Stream s = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\GZipCompressed.txt", FileMode.OpenOrCreate))
+{
+    using (var gzipStream = new GZipStream(s, CompressionLevel.Optimal))
     {
-        using (var gzipStream = new GZipStream(s, CompressionLevel.Optimal))
+        data.CopyTo(gzipStream);
+    }
+}
+
+//Ecrypt the data compress
+using (Stream data = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\GZipCompressed.txt", FileMode.OpenOrCreate))
+
+using (Stream s = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\Encription.txt", FileMode.OpenOrCreate))
+{
+    using (var aes = Aes.Create())
+    {
+
+        using (CryptoStream cryptStream = new CryptoStream(s, aes.CreateEncryptor(), CryptoStreamMode.Write))
         {
-            data.CopyTo(gzipStream);
+            data.CopyTo(cryptStream);
         }
     }
 
-//Ecrypt that data
-using (Stream data = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\data.txt", FileMode.OpenOrCreate))
+}
 
-    using (Stream s = new FileStream(@"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\Encription.txt", FileMode.OpenOrCreate))
-    {
-        using (var aes = Aes.Create()) {
-        
-            using (CryptoStream cryptStream = new CryptoStream(s, aes.CreateEncryptor(), CryptoStreamMode.Write))
-            {
-                data.CopyTo(cryptStream);
-            }
-        }
-    
-    }
+//read the data
+string sourceFile = @"C:\Users\Roxy\Documents\AMDARIS\dev1\publicapp\Streams\Streams\Encription.txt";
+string dataRead = File.ReadAllText(sourceFile);
+WriteLine(dataRead);
+
+
+
 
 
 
---
+++ Streams/Streams/Sport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams
+{
+    public class Sport
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; set; }
+
+        public Sport(string name)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+        }   
+    }
+}
